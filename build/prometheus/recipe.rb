b2eeef0ae65da1f59a7335e84d86d365cf88dd1d
Daniel Kertesz <daniel@spatof.org>
2018-03-18 23:26:13 +0000

    update and fix prometheus

--- build/prometheus/recipe.rb
+++ build/prometheus/recipe.rb
@@ -1,13 +1,14 @@
 class Prometheus < FPM::Cookery::Recipe
   homepage 'https://prometheus.io/'
-  source 'https://github.com/prometheus/prometheus/releases/download/v2.1.0/prometheus-2.1.0.linux-amd64.tar.gz'
-  sha256 'f181f619c9a8e0750c1ac940eb00a0881cc50386d896f06f159e9a5b68db60a0'
+  source 'https://github.com/prometheus/prometheus/releases/download/v2.2.1/prometheus-2.2.1.linux-amd64.tar.gz'
+  sha256 'ec1798dbda1636f49d709c3931078dc17eafef76c480b67751aa09828396cf31'
 
   name 'prometheus'
-  version '2.1.0'
+  version '2.2.1'
   revision '1'
 
   description 'The Prometheus monitoring system and time series database.'
+  maintainer 'Daniel Kertesz <daniel@spatof.org>'
   config_files '/etc/prometheus/prometheus.yml'
   
   pre_install 'pre-install'
